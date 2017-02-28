a9643b7592132a9f17bd24d7e4bf4ca432c9a808
Fay <rebecca.hsu1989@gmail.com>
2017-02-28 21:22:40 +0800

    add fontawesome

--- src/js/create_image.js
+++ src/js/create_image.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../css/reset.css';
+import '../css/font-awesome.min.css';
 import '../css/layout.css';
  
 class CreateImage extends Component {
@@ -8,7 +9,7 @@ class CreateImage extends Component {
     this.state = {
         title:"",
         reason:"",
-        imageUrl:"",
+        imgUrl:"",
         type: "url",
         step:0,
     };
@@ -31,13 +32,15 @@ class CreateImage extends Component {
   }
   render() {
     const defaultTitle = "你有看過這隻貓嗎";
-    const defaultReason = "如果沒有，現在讓你看看牠，牠真的超級可愛";
+    const defaultReason = "這是我們的貓，她真的非常傲嬌 如果你沒看過，現在讓你看一下";
+    const defaultImgUrl = "https://scontent-tpe1-1.xx.fbcdn.net/v/t31.0-8/16798013_10212225654460678_45758179352349449_o.jpg?oh=23ea65f20735456b9c29cebdd702dbfd&oe=59442861";
     return (
         <div className="album-container">
           	<div className="wrapper">
                <StepBox />
     		   <ImageBox title={this.state.title===""?defaultTitle:this.state.title}
-                        reason={this.state.reason===""?defaultReason:this.state.reason}/>
+                        reason={this.state.reason===""?defaultReason:this.state.reason}
+                        imgUrl={this.state.imgUrl===""?defaultImgUrl:this.state.imgUrl}/>
                <GuideText step={this.state.step}/>
     		</div>
         	<div className="bottom-controller">
@@ -46,13 +49,13 @@ class CreateImage extends Component {
         			<div className="panel-body">
         				<form>
         					<div className="form-group">
-        						<label>上方文字&nbsp;:</label>
+        						<label><span className="mark">*</span>上方文字&nbsp;:</label>
         						<textarea type="text" value={this.state.title} name="" 
         								onChange={(e)=>this.handleChange("title",e.target.value)}>
                                 </textarea>
         					</div>
         					<div className="form-group">
-        						<label>選取圖片&nbsp;:</label>
+        						<label><span className="mark">*</span>選取圖片&nbsp;:</label>
                                 <input type="radio" name="type" value="file" 
                                         checked={this.state.type==="file"}
                                         onChange={(e)=>this.handleChange("type",e.target.value)} />上傳檔案
@@ -61,19 +64,19 @@ class CreateImage extends Component {
                                         onChange={(e)=>this.handleChange("type",e.target.value)}/>圖片網址
                                 <div className="form-group">
                                     <div className={this.state.type==="file"?"":"hidden"}>
-                                        <label>上傳檔案&nbsp;:</label>
+                                        <label><span className="mark">*</span>上傳檔案&nbsp;:</label>
                                         <input type="file" value={this.state.url} name="" 
                                             onChange={(e)=>this.handleChange("imageUrl",e.target.value)}/>
                                     </div>
                                     <div className={this.state.type==="file"?"hidden":""}>
-                                        <label>圖片網址&nbsp;:</label>
+                                        <label><span className="mark">*</span>圖片網址&nbsp;:</label>
                                         <input type="text" value={this.state.url} name="" 
                                           onChange={(e)=>this.handleChange("imageUrl",e.target.value)}/>
                                     </div>
                                 </div>
                             </div>               
         					<div className="form-group">
-        						<label>下方文字&nbsp;:</label>
+        						<label><span className="mark">*</span>下方文字&nbsp;:</label>
         						<textarea value={this.state.description} name="" 
         								onChange={(e)=>this.handleChange("reason",e.target.value)}>
         						</textarea>
@@ -107,7 +110,9 @@ class ImageBox extends Component {
 		return (
 		    <div className="image-box">
                 <p className="image-title">{this.props.title}</p>
-                <img alt=""/>
+                <div className="my-image">
+                    <img src={this.props.imgUrl} alt=""/>
+                </div>
                 <p className="image-reason">{this.props.reason}</p>			
 			</div>
 		);
@@ -119,7 +124,9 @@ class GuideText extends Component {
     const guideBox = ["請輸入必要欄位","可以準備輸出圖片囉"];
     return (
         <div className="guide-box">
-            <p>{guideBox[this.props.step]}</p>
+            <p>{guideBox[this.props.step]}
+                <span className="icon-box"><i className="fa fa-question-circle-o"></i></span>
+            </p>
         </div>
     );
   }
