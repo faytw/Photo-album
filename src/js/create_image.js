8959baadad5b1f00489bb43e1c60115f21407534
Fay <rebecca.hsu1989@gmail.com>
2017-03-05 03:32:20 +0800

    finish layout

--- src/js/create_image.js
+++ src/js/create_image.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import '../css/reset.css';
 import '../css/font-awesome.min.css';
 import '../css/layout.css';
- 
+
 class CreateImage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        title:"",
-        reason:"",
-        imgUrl:"",
+        topText:"",
+        bottomText:"",
+        imageUrl:"",
         type: "url",
         step:0,
+        previewButton:false,
+        previewImage:false,
+        finished:false,
+        dataUrl:""
     };
   }
   handleChange(key,value){
@@ -19,29 +23,67 @@ class CreateImage extends Component {
   		[key]:value,
   	});
 
-    if(this.state.title!=="" && this.state.imageUrl!=="" && this.state.reason!==""){
-        this.setState({step:1});
+    if(this.state.topText!=="" && this.state.imageUrl!=="" && this.state.bottomText!==""){
+        this.setState({
+            step:1,
+            previewButton:true,
+        });
     }else{
-        this.setState({step:0});
+        this.setState({
+            step:0,
+            previewButton:false,
+        });
     }
- 
   }
- 
-  addImage(){
-  	//
+  previewImage(){
+  	        const canvas = this.refs.canvas;
+            const img = new Image();
+            const topText = this.state.topText;
+            const bottomText = this.state.bottomText;
+            const context = canvas.getContext('2d');
+        
+            img.onload = function () {
+                context.drawImage(img, 40, 31, 160, 240);
+                context.fillText(topText, 70, 18); 
+                context.fillText(bottomText, 20, 290); 
+            }
+
+            img.src = this.state.imageUrl;
+
+            // const imgOutput = (
+            //     <div>
+            //         <a href="#" download="dl.png"
+            //             onClick={(e)=>location.href=this.refs.canvas.toDataURL()}>下載
+            //         </a>
+            //     </div>
+            //     );
+
+            
+            const dataURL = canvas.toDataURL();
+            this.refs.download.href = dataURL;
+
+            // console.log("imgOutput:"+imgOutput);
+      
+            this.setState({
+                step:2,
+                finished:true,
+                previewImage:true,
+            });
   }
   render() {
-    const defaultTitle = "你有看過這隻貓嗎";
-    const defaultReason = "這是我們的貓，她真的非常傲嬌 如果你沒看過，現在讓你看一下";
-    const defaultImgUrl = "https://scontent-tpe1-1.xx.fbcdn.net/v/t31.0-8/16798013_10212225654460678_45758179352349449_o.jpg?oh=23ea65f20735456b9c29cebdd702dbfd&oe=59442861";
     return (
         <div className="album-container">
           	<div className="wrapper">
                <StepBox />
-    		   <ImageBox title={this.state.title===""?defaultTitle:this.state.title}
-                        reason={this.state.reason===""?defaultReason:this.state.reason}
-                        imgUrl={this.state.imgUrl===""?defaultImgUrl:this.state.imgUrl}/>
-               <GuideText step={this.state.step}/>
+    		    <div className={this.state.previewImage?"image-box":"no-image"}>
+                    <canvas ref="canvas" width={240} height={320} />
+                    <a href="#" ref="download" download="test.png" onClick={(e)=>require(this.state.dataUrl)}>DOWNLOAD</a>
+                </div>
+               <GuideText step={this.state.step} 
+                        preview={this.state.previewButton}
+                        finished={this.state.finished} 
+                        download={this.state.dataUrl}
+                        onClick={(e)=>this.previewImage()}/>
     		</div>
         	<div className="bottom-controller">
         		<span className="up-panel-button"></span>
@@ -49,36 +91,34 @@ class CreateImage extends Component {
         			<div className="panel-body">
         				<form>
         					<div className="form-group">
-        						<label><span className="mark">*</span>上方文字&nbsp;:</label>
-        						<textarea type="text" value={this.state.title} name="" 
-        								onChange={(e)=>this.handleChange("title",e.target.value)}>
-                                </textarea>
-        					</div>
-        					<div className="form-group">
         						<label><span className="mark">*</span>選取圖片&nbsp;:</label>
                                 <input type="radio" name="type" value="file" 
-                                        checked={this.state.type==="file"}
-                                        onChange={(e)=>this.handleChange("type",e.target.value)} />上傳檔案
-                                <input type="radio" name="type" value="url" 
-                                        defaultChecked={this.state.type==="url"}
-                                        onChange={(e)=>this.handleChange("type",e.target.value)}/>圖片網址
+                                        onChange={(e)=>this.handleChange("type",e.target.value)} />
+                                        <span className={this.state.type==="file"?"checked":""}>上傳檔案</span>
+                                <input className="ml-style" type="radio" name="type" value="url"
+                                        defaultChecked="true"
+                                        onChange={(e)=>this.handleChange("type",e.target.value)}/>
+                                        <span className={this.state.type==="url"?"checked":""}>圖片網址</span>
                                 <div className="form-group">
-                                    <div className={this.state.type==="file"?"":"hidden"}>
-                                        <label><span className="mark">*</span>上傳檔案&nbsp;:</label>
-                                        <input type="file" value={this.state.url} name="" 
-                                            onChange={(e)=>this.handleChange("imageUrl",e.target.value)}/>
-                                    </div>
-                                    <div className={this.state.type==="file"?"hidden":""}>
-                                        <label><span className="mark">*</span>圖片網址&nbsp;:</label>
-                                        <input type="text" value={this.state.url} name="" 
-                                          onChange={(e)=>this.handleChange("imageUrl",e.target.value)}/>
-                                    </div>
+                                    <label>
+                                        <span className="mark">*</span>
+                                        {this.state.type==="file"?"上傳檔案":"圖片網址"}&nbsp;:
+                                    </label>
+                                    <input type={this.state.type==="file"?"file":"text"} 
+                                        value={this.state.imageUrl} name=""
+                                        onChange={(e)=>this.handleChange("imageUrl",e.target.value)}/>
                                 </div>
+                            </div>
+                            <div className="form-group">
+                                <label><span className="mark">*</span>上方文字&nbsp;:</label>
+                                <textarea type="text" value={this.state.topText} name=""
+                                        onChange={(e)=>this.handleChange("topText",e.target.value)}>
+                                </textarea>
                             </div>               
         					<div className="form-group">
         						<label><span className="mark">*</span>下方文字&nbsp;:</label>
-        						<textarea value={this.state.description} name="" 
-        								onChange={(e)=>this.handleChange("reason",e.target.value)}>
+        						<textarea value={this.state.bottomText} name=""
+        								onChange={(e)=>this.handleChange("bottomText",e.target.value)}>
         						</textarea>
         					</div>
         			    </form>
@@ -105,27 +145,20 @@ class StepBox extends Component {
   }
 }
 
-class ImageBox extends Component {
-	render(){
-		return (
-		    <div className="image-box">
-                <p className="image-title">{this.props.title}</p>
-                <div className="my-image">
-                    <img src={this.props.imgUrl} alt=""/>
-                </div>
-                <p className="image-reason">{this.props.reason}</p>			
-			</div>
-		);
-	}
-}
-
 class GuideText extends Component {
   render(){
-    const guideBox = ["請輸入必要欄位","可以準備輸出圖片囉"];
+    const guideBox = ["請輸入必要欄位","點擊預覽","完成！"];
     return (
         <div className="guide-box">
             <p>{guideBox[this.props.step]}
-                <span className="icon-box"><i className="fa fa-question-circle-o"></i></span>
+                <span className="icon-box" onClick={this.props.preview?this.props.onClick:null}>
+                    <i className={this.props.preview?this.props.finished?"hidden":"fa fa-eye":"fa fa-question-circle-o"}></i>
+                </span>
+               
+               
+                <span className={this.props.finished?"icon-box":"hidden"}>
+                    或&nbsp;&nbsp;分享至<i className="fa fa-facebook-square"></i>
+                </span>
             </p>
         </div>
     );
