2be61c075f7a3b58931c229d3777f65c5797744c
Fay <rebecca.hsu1989@gmail.com>
2017-03-14 14:10:14 +0800

    移除下載圖片，加上RWD,  新增流程顯示

--- src/js/create_image.js
+++ src/js/create_image.js
@@ -15,7 +15,6 @@ class CreateImage extends Component {
         previewButton:false,
         previewImage:false,
         finished:false,
-        dataUrl:""
     };
   }
   handleChange(key,value){
@@ -42,28 +41,15 @@ class CreateImage extends Component {
             const bottomText = this.state.bottomText;
             const context = canvas.getContext('2d');
         
+           
             img.onload = function () {
                 context.drawImage(img, 40, 31, 160, 240);
                 context.fillText(topText, 70, 18); 
                 context.fillText(bottomText, 20, 290); 
             }
-
+          
             img.src = this.state.imageUrl;
-
-            // const imgOutput = (
-            //     <div>
-            //         <a href="#" download="dl.png"
-            //             onClick={(e)=>location.href=this.refs.canvas.toDataURL()}>下載
-            //         </a>
-            //     </div>
-            //     );
-
-            
-            const dataURL = canvas.toDataURL();
-            this.refs.download.href = dataURL;
-
-            // console.log("imgOutput:"+imgOutput);
-      
+           
             this.setState({
                 step:2,
                 finished:true,
@@ -71,15 +57,15 @@ class CreateImage extends Component {
             });
   }
   render() {
+   
     return (
         <div className="album-container">
           	<div className="wrapper">
-               <StepBox />
+                <StepBox step={this.state.step}/>
     		    <div className={this.state.previewImage?"image-box":"no-image"}>
                     <canvas ref="canvas" width={240} height={320} />
-                    <a href="#" ref="download" download="test.png" onClick={(e)=>require(this.state.dataUrl)}>DOWNLOAD</a>
                 </div>
-               <GuideText step={this.state.step} 
+                <GuideText step={this.state.step} 
                         preview={this.state.previewButton}
                         finished={this.state.finished} 
                         download={this.state.dataUrl}
@@ -91,12 +77,13 @@ class CreateImage extends Component {
         			<div className="panel-body">
         				<form>
         					<div className="form-group">
-        						<label><span className="mark">*</span>選取圖片&nbsp;:</label>
+        						<label className="mr-style"><span className="mark">*</span>選取圖片&nbsp;:</label>
                                 <input type="radio" name="type" value="file" 
                                         onChange={(e)=>this.handleChange("type",e.target.value)} />
                                         <span className={this.state.type==="file"?"checked":""}>上傳檔案</span>
                                 <input className="ml-style" type="radio" name="type" value="url"
                                         defaultChecked="true"
+                                        disabled={this.state.step!==0}
                                         onChange={(e)=>this.handleChange("type",e.target.value)}/>
                                         <span className={this.state.type==="url"?"checked":""}>圖片網址</span>
                                 <div className="form-group">
@@ -106,18 +93,21 @@ class CreateImage extends Component {
                                     </label>
                                     <input type={this.state.type==="file"?"file":"text"} 
                                         value={this.state.imageUrl} name=""
+                                        disabled={this.state.step!==0}
                                         onChange={(e)=>this.handleChange("imageUrl",e.target.value)}/>
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label><span className="mark">*</span>上方文字&nbsp;:</label>
                                 <textarea type="text" value={this.state.topText} name=""
+                                        disabled={this.state.step!==0}
                                         onChange={(e)=>this.handleChange("topText",e.target.value)}>
                                 </textarea>
                             </div>               
         					<div className="form-group">
         						<label><span className="mark">*</span>下方文字&nbsp;:</label>
         						<textarea value={this.state.bottomText} name=""
+                                        disabled={this.state.step!==0}
         								onChange={(e)=>this.handleChange("bottomText",e.target.value)}>
         						</textarea>
         					</div>
@@ -135,11 +125,11 @@ class StepBox extends Component {
   render(){
     return (
         <div className="step-box">
-            <span>新增圖片</span>
+            <span className={this.props.step===0?"this-step":null}>step1:新增圖片</span>
             <span>>></span>
-            <span>預覽新圖片</span>
+            <span className={this.props.step===1?"this-step":null}>step2:預覽新圖片</span>
             <span>>></span>
-            <span>輸出圖片</span>
+            <span className={this.props.step===2?"this-step":null}>step3:另存圖片</span>
         </div>
     );
   }
@@ -147,17 +137,12 @@ class StepBox extends Component {
 
 class GuideText extends Component {
   render(){
-    const guideBox = ["請輸入必要欄位","點擊預覽","完成！"];
+    const guideBox = ["請輸入必要欄位","點擊預覽","完成"];
     return (
         <div className="guide-box">
             <p>{guideBox[this.props.step]}
                 <span className="icon-box" onClick={this.props.preview?this.props.onClick:null}>
-                    <i className={this.props.preview?this.props.finished?"hidden":"fa fa-eye":"fa fa-question-circle-o"}></i>
-                </span>
-               
-               
-                <span className={this.props.finished?"icon-box":"hidden"}>
-                    或&nbsp;&nbsp;分享至<i className="fa fa-facebook-square"></i>
+                    <i className={this.props.preview?this.props.finished?"fa  fa-graduation-cap":"fa fa-eye":"fa fa-question-circle-o"}></i>
                 </span>
             </p>
         </div>
